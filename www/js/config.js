b69090b90dc1e026d066f4ff6f7973e0a1a57493
ZH1995 <acmer_doubleq@qq.com>
2016-05-08 14:49:09 +0800

    create app
    create app

---
+++ www/js/config.js
@@ -0,0 +1,12 @@
+/**
+ * Created by QQ on 2016/5/7.
+ */
+angular.module('starter.config',[])
+
+.constant('ENV', {
+  'debug': false,
+  'api': "http://www.phonegap100.com/appapi.php",
+  'siteUrl': "http://www.phonegap100.com",
+  'imgUrl': "http://www.phonegap100.com/data/attachment",
+  'version': '1.0.1'
+})
